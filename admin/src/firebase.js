1370866379862fcfc0ec043f72cdbdd8535b166f
mauricioaguirre <e-duam@outlook.com>
2021-12-22 19:05:23 -0500

    PF - 2da parte

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCL8Lnv3NZJeHYB5Slp07_LYYoIDD1NEbg",
+  authDomain: "shop-ec2d2.firebaseapp.com",
+  projectId: "shop-ec2d2",
+  storageBucket: "shop-ec2d2.appspot.com",
+  messagingSenderId: "712121576081",
+  appId: "1:712121576081:web:9f4e0185869e41359cad30"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
